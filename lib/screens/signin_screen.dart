b33bb4640a753aae2f2050dce0cf4735f57bb312
dadikas <73420676+dadikas@users.noreply.github.com>
2024-03-03 16:54:06 +0700

    Signin

---
+++ lib/screens/signin_screen.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/material.dart';
+import 'package:student_hub/components/custom_button.dart';
+import 'package:student_hub/components/custom_appbar.dart';
+import 'package:student_hub/components/custom_textfield.dart';
+import 'package:student_hub/components/initial_body.dart';
+import 'package:student_hub/utils/spacing_util.dart';
+import 'package:student_hub/components/custom_text.dart';
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+
+class _SignInScreenState extends State<SignInScreen> {
+  final emailController = TextEditingController();
+  void submit(){}
+  void onPressed(){}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppbar(onPressed: onPressed, currentContext: context),
+      body: InitialBody(
+        child: Column(
+          children: [
+            const Center(child: CustomText(
+              text: 'Log in with StudentHub',
+              isBold: true,
+            )),
+            const SizedBox(
+              height: SpacingUtil.largeHeight,
+            ),
+            CustomTextfield(controller: emailController, hintText: 'email'),
+            const SizedBox(
+              height: SpacingUtil.mediumHeight,
+            ),
+            CustomTextfield(controller: emailController, hintText: 'password', obscureText: true),
+            const SizedBox(
+              height: SpacingUtil.mediumHeight,
+            ),
+            CustomButton(onPressed: submit, text: 'sign in'),
+            const SizedBox(
+              height: 400,
+            ),
+            const Center(
+              child: CustomText(
+                text:
+                '__Don\'t have an Student Hub account?__',
+                isCenter: true,
+              ),
+            ),
+            CustomButton(onPressed: submit, text: 'sign up'),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
