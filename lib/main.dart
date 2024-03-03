1e6a42965574fc6fb2e459e9d2f95c5343c96933
LuuMinhNhat <nguoidep433@gmail.com>
2024-03-03 19:51:44 +0700

    sign_up_setup1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:student_hub/screens/registration_two_student_screen.dart';
-import 'package:student_hub/screens/signupsetup1.dart';
+import 'package:student_hub/screens/signupsetup1_screen.dart';
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
