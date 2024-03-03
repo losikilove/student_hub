cc97808ed38db8e70dff80ce556666876d08775d
losikilove <75561119+losikilove@users.noreply.github.com>
2024-03-03 19:46:29 +0700

    edit signupsetup1 screen

---
+++ lib/models/enums/enum_user.dart
@@ -0,0 +1,1 @@
+enum EnumUser { student, company }
--- lib/screens/signupsetup1.dart
+++ lib/screens/signupsetup1.dart
@@ -1,16 +1,12 @@
-
-
 import 'package:flutter/material.dart';
-
 import 'package:student_hub/components/custom_anchor.dart';
 import 'package:student_hub/components/custom_text.dart';
-import 'package:student_hub/utils/color_util.dart';
+import 'package:student_hub/components/initial_body.dart';
+import 'package:student_hub/models/enums/enum_user.dart';
 import 'package:student_hub/utils/spacing_util.dart';
-import '../components/custom_button.dart';
-import 'package:roundcheckbox/roundcheckbox.dart';
+import 'package:student_hub/components/custom_button.dart';
+import 'package:student_hub/components/custom_appbar.dart';
 
-
-import '../components/custom_appbar.dart';
 class SignUpSetup1 extends StatefulWidget {
   const SignUpSetup1({super.key});
 
@@ -19,103 +15,94 @@ class SignUpSetup1 extends StatefulWidget {
 }
 
 class _SignUpSetup1State extends State<SignUpSetup1> {
-  bool checkbox1Value = false;
-  bool checkbox2Value = false;
+  EnumUser? _user = EnumUser.student;
+
+  void chooseUserType(EnumUser? value) {
+    setState(() {
+      _user = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: ColorUtil.lightPrimary,
-       appBar: CustomAppbar(
-        onPressed: (){},
+      appBar: CustomAppbar(
+        onPressed: () {},
         currentContext: context,
       ),
-      body: Column(
+      body: InitialBody(
+        child: Column(
           children: [
-            SizedBox(
-              height: SpacingUtil.mediumHeight,
-            ),
-            Center(child: CustomText(text:"Join as Company or Student",isBold: true,)),
-      
-            SizedBox(
-              height: SpacingUtil.mediumHeight,
-            ),
-            Container(
-              height: 200,
-              child: ListView(
-                children: [
-                  Container(
-      margin:const EdgeInsets.only(left:9,right:9,bottom: 12.0),
-      child: ListTile(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        tileColor: Colors.white,
-        subtitle: const Text("I am a company,find engineer for project",style: TextStyle(fontSize: 16),),
-        leading: const Icon(Icons.account_box_outlined,size: 30,),
-        trailing: RoundCheckBox(
-          isChecked:checkbox1Value ,
-          onTap: (bool? value ) {
-             if (value != null) {
-             setState(() {
-              checkbox1Value = value;
-              checkbox2Value = !value;
-            });
-            }
-          },
-          size: 30,
-          uncheckedColor: Colors.white,
-          checkedColor: Colors.blue,
-        ),
-      ),
-    ),
-    Container(
-      margin:const EdgeInsets.only(left:9,right:9,bottom: 12.0),
-      child: ListTile(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-        tileColor: Colors.white,
-        subtitle: const Text("I am a company,find engineer for project",style: TextStyle(fontSize: 16),),
-        leading: const Icon(Icons.account_box_outlined,size: 30,),
-        trailing: RoundCheckBox(
-          isChecked: checkbox2Value,
-          onTap: (bool? value) {
-            if (value != null) {
-             setState(() {
-              checkbox2Value = value;
-              checkbox1Value = !value;
-            });
-            }
-          
-              
-          },
-          size: 30,
-          uncheckedColor: Colors.white,
-          checkedColor: Colors.blue,
-        ),
-      ),
-    )
-                ], 
-               ),
-            ),
-           
-              
-              Container(
-                margin:const EdgeInsets.only(bottom: 17.0,left: 10.0,right: 10.0),
-                child: CustomButton(text: "Create account",
-                onPressed: (){},
-                size: CustomButtonSize.large,
+            // Title text
+            const Center(
+              child: CustomText(
+                text: "Join as Company or Student",
+                isBold: true,
               ),
-                
             ),
-               Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Already have an account ? "),
-                    CustomAnchor(text: "Log in", onTap: (){})
-                ],
-            )
+            const SizedBox(
+              height: SpacingUtil.mediumHeight,
+            ),
+            // Card of company introducing
+            _cardChoice(Icons.account_box, EnumUser.company,
+                'I am a Company, find engineer for project'),
+            const SizedBox(
+              height: SpacingUtil.smallHeight,
+            ),
+            // Card of student introducing
+            _cardChoice(Icons.accessibility, EnumUser.student,
+                'I am a Student, find a project'),
+            const SizedBox(
+              height: SpacingUtil.mediumHeight,
+            ),
+            CustomButton(
+              onPressed: () {},
+              text: 'Create account',
+              size: CustomButtonSize.large,
+            ),
+            const SizedBox(
+              height: SpacingUtil.mediumHeight,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CustomText(text: 'Already have an account? '),
+                CustomAnchor(text: "Log in", onTap: () {})
+              ],
+            ),
           ],
         ),
+      ),
+    );
+  }
 
-
+  // create card widget which is unique of this screen
+  Widget _cardChoice(IconData icon, EnumUser user, String intro) {
+    return Card(
+      shape: Border.all(),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(icon),
+                Radio<EnumUser>(
+                  activeColor: Colors.black,
+                  value: user,
+                  groupValue: _user,
+                  onChanged: chooseUserType,
+                ),
+              ],
+            ),
+            CustomText(
+              text: intro,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
