94c428f138d017fdc480afcecbdfb68a031ce22d
losikilove <75561119+losikilove@users.noreply.github.com>
2024-03-05 21:29:03 +0700

    edit initial_body

--- lib/components/custom_listtile.dart
+++ lib/components/custom_listtile.dart
@@ -1,23 +1,26 @@
-
 import 'package:flutter/material.dart';
 
-class CustomListTitle extends StatelessWidget{
+class CustomListTitle extends StatelessWidget {
   final void Function()? onTap;
   final String text;
   final String? subtext;
   final IconData icon;
-  const CustomListTitle({required this.icon,required this.text,required this.onTap, required this.subtext});
+  const CustomListTitle(
+      {super.key,
+      required this.icon,
+      required this.text,
+      required this.onTap,
+      required this.subtext});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
         border: Border(
-          bottom: BorderSide(
-            color: Color.fromARGB(255, 0, 0, 0), 
-            width: 2,
-          )
-        ),
+            bottom: BorderSide(
+          color: Color.fromARGB(255, 0, 0, 0),
+          width: 2,
+        )),
       ),
       child: ListTile(
         leading: Icon(icon, size: 50),
--- lib/components/initial_body.dart
+++ lib/components/initial_body.dart
@@ -3,14 +3,25 @@ import 'package:student_hub/utils/color_util.dart';
 
 class InitialBody extends StatelessWidget {
   final Widget child;
-  const InitialBody({super.key, required this.child});
+  final double left;
+  final double right;
+  final double top;
+  final double bottom;
+  const InitialBody(
+      {super.key,
+      required this.child,
+      this.left = 18.0,
+      this.right = 18.0,
+      this.top = 36.0,
+      this.bottom = 0.0});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: ColorUtil.lightPrimary,
       child: Padding(
-        padding: const EdgeInsets.only(left: 18.0, right: 18.0, top: 36.0),
+        padding:
+            EdgeInsets.only(left: left, right: right, top: top, bottom: bottom),
         child: child,
       ),
     );
