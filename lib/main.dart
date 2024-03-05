2d1b6f8bfa145eec663abc6a54e34e896e96a97b
dadikas <73420676+dadikas@users.noreply.github.com>
2024-03-05 16:14:34 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:student_hub/screens/Dashboard.dart';
+import 'package:student_hub/screens/companyprofile_screen.dart';
 import 'package:student_hub/screens/registration_two_company_screen.dart';
 import 'package:student_hub/screens/registration_two_student_screen.dart';
 import 'package:student_hub/screens/signupsetup1_screen.dart';
@@ -7,6 +9,6 @@ import 'package:student_hub/screens/switch_account.dart';
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: SignUpSetup1(),
+    home: Dashboard(),
   ));
 }
