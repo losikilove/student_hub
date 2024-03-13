b0a4ba4bd48a2c0b900d76263c8148e4c729e2be
dadikas <73420676+dadikas@users.noreply.github.com>
2024-03-13 19:10:50 +0700

    updateProjectModel_complete_like

--- lib/models/project_model.dart
+++ lib/models/project_model.dart
@@ -2,8 +2,8 @@ class ProjectModel {
   final String _title;
   final String _timeCreating;
   final List<String> _wishes;
-  final bool isCompelte;
-  final bool like;
+  bool _isCompelte;
+  bool _like;
   int _numberProposals;
   int _numberMessages;
   int _numberHires;
@@ -12,25 +12,21 @@ class ProjectModel {
     this._title,
     this._timeCreating,
     this._wishes,
-    this.isCompelte,
-    this.like,
+    this._isCompelte,
+    this._like,
     this._numberProposals,
     this._numberMessages,
     this._numberHires,
   );
-  ProjectModel.origin():
-    this._title = '',
-    this._timeCreating = '',
-    this._wishes = [],
-    this.isCompelte = false,
-    this.like = false,
-    this._numberProposals = 0,
-    this._numberMessages = 0,
-    this._numberHires =  0;
+  set setLike(bool flag){
+    _like = flag;
+  }
 
   String get title => _title;
   String get timeCreating => _timeCreating;
   List<String> get wishes => _wishes;
+  bool get isCompelte => _isCompelte;
+  bool get like => _like;
   int get numberProposals => _numberProposals;
   int get numberMessages => _numberMessages;
   int get numberHires => _numberHires;
