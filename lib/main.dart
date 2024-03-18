ba180c5b95599d0cc1f139734ed24f56e07b8ee9
dadikas <73420676+dadikas@users.noreply.github.com>
2024-03-18 19:10:11 +0700

    Use Dash chat 2 and add MessBodyPartChat

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:student_hub/screens/browse_project_detail_screen.dart';
+import 'package:student_hub/screens/content_body/main_screen/message_body.dart';
 import 'package:student_hub/screens/main_screen.dart';
 import 'package:student_hub/screens/companyprofile_screen.dart';
 import 'package:student_hub/screens/profile_student_step1_screen.dart';
@@ -15,6 +16,6 @@ import 'package:student_hub/screens/switch_account_screen.dart';
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: SettingsScreen(),
+    home: MessageBodyPartChat(),
   ));
 }
--- lib/screens/content_body/main_screen/message_body.dart
+++ lib/screens/content_body/main_screen/message_body.dart
@@ -1,3 +1,4 @@
+import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -148,5 +149,42 @@ class _MessageBody extends State<MessageBody> {
         )
       ],
     );
+  }
+}
+
+class MessageBodyPartChat extends StatefulWidget {
+  const MessageBodyPartChat({super.key});
+
+  @override
+  State<MessageBodyPartChat> createState() => _MessageBodyPartChatState();
+}
+
+class _MessageBodyPartChatState extends State<MessageBodyPartChat> {
+  final ChatUser _currentUser = ChatUser(id: '1', firstName: 'phat',lastName: 'bo');
+  final ChatUser _anotherUser = ChatUser(id: '2', firstName: 'nhat',lastName: 'bo');
+  List<ChatMessage> _message = <ChatMessage>[];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppbar(
+        title: 'chat',
+        onPressed: () {
+          
+        },
+        currentContext: context,
+      ),
+      body: DashChat(
+        currentUser: _currentUser,
+        onSend: (ChatMessage message){
+          getChatResponse(message);
+        },
+        messages: _message,
+        ),
+    );
+  }
+  Future<void> getChatResponse(ChatMessage message) async {
+    setState(() {
+      _message.insert(0, message);
+    });
   }
 }
