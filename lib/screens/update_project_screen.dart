f3ab0ae2fd449ac5cbf292cfa168d7b90c5f5a89
losikilove <75561119+losikilove@users.noreply.github.com>
2024-04-13 13:30:33 +0700

    hot fix: add logical usercase into update project

--- lib/screens/update_project_screen.dart
+++ lib/screens/update_project_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:student_hub/components/custom_appbar.dart';
 import 'package:student_hub/components/custom_button.dart';
 import 'package:student_hub/components/custom_text.dart';
 import 'package:student_hub/components/initial_body.dart';
+import 'package:student_hub/components/popup_notification.dart';
 import 'package:student_hub/models/enums/enum_projectlenght.dart';
 import 'package:student_hub/models/project_company_model.dart';
 import 'package:student_hub/screens/main_screen.dart';
 import 'package:student_hub/services/project_service.dart';
+import 'package:student_hub/utils/api_util.dart';
 import 'package:student_hub/utils/navigation_util.dart';
 import 'package:student_hub/utils/spacing_util.dart';
 
@@ -30,20 +33,44 @@ class _UpdateProjectScreenState extends State<UpdateProjectScreen> {
     });
   }
 
-  Future<void> UpdateProject() async {
-    widget.projectCModel.title = 
-      controllerTile.text;
+  Future<void> updateProject() async {
+    final confirmedUpdatedProject = await _showDialogConfirmUpdatedCProject();
+
+    if (confirmedUpdatedProject == null || confirmedUpdatedProject == false)
+      return;
+
+    widget.projectCModel.title = controllerTile.text;
     widget.projectCModel.numberofStudent =
-      int.parse(controllerNumberOfStudent.text);
-    widget.projectCModel.description = 
-      controllerDescription.text;
-    widget.projectCModel.projectScopeFlag = 
-      _projectDuration;
-    await ProjectService.updateProject(
+        int.parse(controllerNumberOfStudent.text);
+    widget.projectCModel.description = controllerDescription.text;
+    widget.projectCModel.projectScopeFlag = _projectDuration;
+
+    final response = await ProjectService.updateProject(
       project: widget.projectCModel,
       context: context,
     );
-    NavigationUtil.toMainScreen(context, MainScreenIndex.dashboard);
+
+    if (response.statusCode == StatusCode.ok.code) {
+      await popupNotification(
+        context: context,
+        type: NotificationType.success,
+        content: 'Update project success',
+        textSubmit: 'Ok',
+        submit: null,
+      );
+
+      NavigationUtil.toMainScreen(context, MainScreenIndex.dashboard);
+      return;
+    }
+
+    // when expired token
+    if (response.statusCode == StatusCode.unauthorized.code) {
+      ApiUtil.handleExpiredToken(context: context);
+      return;
+    }
+
+    //others
+    ApiUtil.handleOtherStatusCode(context: context);
   }
 
   @override
@@ -70,13 +97,13 @@ class _UpdateProjectScreenState extends State<UpdateProjectScreen> {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                CustomText(text: 'Update Project'),
-                SizedBox(
+                const CustomText(text: 'Update Project'),
+                const SizedBox(
                   height: SpacingUtil.mediumHeight,
                 ),
                 TextField(
                   controller: controllerTile,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Project name',
                     hintText: "Project name",
                     border: OutlineInputBorder(
@@ -93,18 +120,21 @@ class _UpdateProjectScreenState extends State<UpdateProjectScreen> {
                 ),
 
                 TextField(
-                    controller: controllerNumberOfStudent,
-                    decoration: InputDecoration(
-                      labelText: 'Number of student',
-                      hintText: "Number of student",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                        borderSide: BorderSide(color: Colors.black),
-                      ),
-                      fillColor: Colors.white,
-                      filled: true,
-                      hoverColor: Colors.black,
-                    )),
+                  controller: controllerNumberOfStudent,
+                  decoration: const InputDecoration(
+                    labelText: 'Number of student',
+                    hintText: "Number of student",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hoverColor: Colors.black,
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                ),
                 const CustomText(
                   text: 'How long will your project take?',
                   isBold: true,
@@ -163,7 +193,7 @@ class _UpdateProjectScreenState extends State<UpdateProjectScreen> {
                 ),
                 TextField(
                   controller: controllerDescription,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Description',
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(10.0)),
@@ -194,13 +224,39 @@ class _UpdateProjectScreenState extends State<UpdateProjectScreen> {
             ),
             Expanded(
               child: CustomButton(
-                onPressed: () {
-                  UpdateProject();
-                },
+                onPressed: updateProject,
                 text: 'Update',
               ),
             ),
           ],
         ));
   }
+
+  Future<bool?> _showDialogConfirmUpdatedCProject() => showDialog<bool?>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text(
+            'WARNING',
+            style: TextStyle(color: Colors.red),
+          ),
+          content: const CustomText(
+            text: 'Are you sure to update your project?',
+          ),
+          actions: [
+            CustomButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              text: 'Yes',
+            ),
+            CustomButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              text: 'No',
+            ),
+          ],
+        );
+      });
 }
