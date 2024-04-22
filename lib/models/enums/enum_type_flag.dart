c63234281f6a3ec47a58e8df9e4cded158138ed8
dadikas <73420676+dadikas@users.noreply.github.com>
2024-04-22 18:41:37 +0700

    add project typeFlag: new

--- lib/models/enums/enum_type_flag.dart
+++ lib/models/enums/enum_type_flag.dart
@@ -1,12 +1,16 @@
 enum EnumTypeFlag {
-  working(value: 0),
-  archive(value: 1);
+  newproject(value: 0),
+  working(value: 1),
+  archive(value: 2);
 
   final int value;
 
   const EnumTypeFlag({required this.value});
 
   static EnumTypeFlag toTypeFlag(int id) {
+    if (id == EnumTypeFlag.newproject.value) {
+      return EnumTypeFlag.newproject;
+    }
     if (id == EnumTypeFlag.working.value) {
       return EnumTypeFlag.working;
     }
